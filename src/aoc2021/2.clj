344ef94ab1f8fd5dee35df0e7a4dd26b17e8076b
Timo Pick <t.pick@tarent.de>
2021-12-04 00:51:10 +0100

    day 2 pt 1 done

---
+++ src/aoc2021/2.clj
@@ -0,0 +1,22 @@
+(ns aoc2021.2)
+
+(require '[clojure.string :as str])
+(require '[clojure.java.io :as io])
+(require '[clojure.core.match :refer [match]])
+
+(def input
+  (str/split
+   (slurp (io/resource "2/input")) #"\n"))
+
+(defn reducefn [[x y] line]
+  (let [spline (str/split line #"\s")]
+    (match [(nth spline 0) (Integer/parseInt (nth spline 1))]
+      ["forward" i] [(+ x i) y]
+      ["up" i] [x (- y i)]
+      ["down" i] [x (+ y i)])))
+
+(reduce *
+    (reduce reducefn [0 0] input)
+)
+
+; part 2
