23565c9581d92048a3ff6c48948c2294d991f8b8
Timo Pick <t.pick@tarent.de>
2021-12-06 00:05:21 +0100

    day3 part 2 WIP
    Determination of the most common bit is not considering reduction of sample size yet, therefor broken

--- src/aoc2021/3.clj
+++ src/aoc2021/3.clj
@@ -20,29 +20,64 @@
    input))
 
 ; 1 if bit-count at position x >= input size / 2 
-(defn gamma-rate [input]
+(defn most-common-bits [input]
   (map {false 0 true 1}
        (map >=
             (count-bits input)
             (repeat (count (first input)) (/ (count input) 2)))))
 
-; inverse of gamma-rate
-(defn epsilon-rate [gamma] 
+; inverse of most-common-bits
+(defn least-common-bits [gamma] 
   (map - (repeat (count gamma) 1) gamma))
 
 ; convert bits-seq to 2r representation and eval
 (defn conv [bits-seq] 
   (read-string (apply str "2r" bits-seq)))
 
-(conv (gamma-rate example-input)) ; 22
-(conv (epsilon-rate (gamma-rate example-input))) ; 9
+(conv (most-common-bits example-input)) ; 22
+(conv (least-common-bits (most-common-bits example-input))) ; 9
 
 (def input
   (str/split
    (slurp (io/resource "3/input")) #"\n"))
 
 (defn solve_part1 []
-  (* (conv (gamma-rate input))
-     (conv (epsilon-rate (gamma-rate input)))))
+  (* (conv (most-common-bits input))
+     (conv (least-common-bits (most-common-bits input)))))
 
 (println "Day 3 #1: " (solve_part1))
+
+; part 2
+
+(defn parse-input [input]
+  (map parse input))
+
+(defn filter-nth-bit
+  [input pos val] 
+  (filter
+   (fn [x]
+     (= (nth x pos) val))
+   input))
+
+(comment
+  ; example from the site
+  (def mcb (most-common-bits example-input))
+  (->
+   (filter-nth-bit (parse-input example-input) 0 (nth mcb 0))
+   (filter-nth-bit 1 (nth mcb 1))
+   (filter-nth-bit 2 (nth mcb 2))
+   (filter-nth-bit 3 (nth mcb 3))
+   (filter-nth-bit 4 (nth mcb 4)))
+  )
+
+(defn with-indexes [seq] (map #(vector %1 %2) (range) seq))
+
+(defn bitmask-filter [input mask] 
+  (reduce
+   (fn [coll [pos val]]
+     (prn coll)
+     (filter-nth-bit coll pos val)) 
+   input
+   (with-indexes mask)))
+
+(bitmask-filter (parse-input example-input) mcb)
