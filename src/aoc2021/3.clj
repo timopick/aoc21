91db8f055f2f7f1094eb3886694cbf4d827483b8
Timo Pick <t.pick@tarent.de>
2021-12-08 14:50:39 +0100

    Solved day3 part2

--- src/aoc2021/3.clj
+++ src/aoc2021/3.clj
@@ -3,81 +3,91 @@
 (require '[clojure.string :as str])
 (require '[clojure.java.io :as io])
 
-(def example-input
-  ["00100" "11110" "10110" "10111"
-   "10101" "01111" "00111" "11100"
-   "10000" "11001" "00010" "01010"])
-
-(defn parse [line]
+(defn parse-line [line]
   (map #(Character/getNumericValue %) line))
 
+(defn parse-input [input]
+  (map parse-line input))
+
+(def example-input
+  (parse-input ["00100" "11110" "10110" "10111"
+                "10101" "01111" "00111" "11100"
+                "10000" "11001" "00010" "01010"]))
+
 ; count bits of each binary column
-(defn count-bits [input]
+(defn count-bits [input-seq]
   (reduce
-   (fn [coll line]
-     (map + coll (parse line)))
-   (repeat (count (first input)) 0) ; initial coll vector
-   input))
+   (fn [coll row]
+     (map + coll row))
+   (repeat (count (first input-seq)) 0) ; initial coll vector
+   input-seq))
 
 ; 1 if bit-count at position x >= input size / 2 
-(defn most-common-bits [input]
+(defn most-common-bits [input-seq]
   (map {false 0 true 1}
        (map >=
-            (count-bits input)
-            (repeat (count (first input)) (/ (count input) 2)))))
+            (count-bits input-seq)
+            (repeat (count (first input-seq)) (/ (count input-seq) 2)))))
 
-; inverse of most-common-bits
-(defn least-common-bits [gamma] 
-  (map - (repeat (count gamma) 1) gamma))
+(most-common-bits example-input)
+
+; 1 if bit-count at position x >= input size / 2 
+(defn least-common-bits [input-seq]
+  (map -
+       (repeat (count (first input-seq)) 1)
+       (most-common-bits input-seq)))
+
+(least-common-bits example-input)
 
 ; convert bits-seq to 2r representation and eval
-(defn conv [bits-seq] 
+(defn conv [bits-seq]
   (read-string (apply str "2r" bits-seq)))
 
-(conv (most-common-bits example-input)) ; 22
-(conv (least-common-bits (most-common-bits example-input))) ; 9
+(conv
+ (most-common-bits example-input)) ; 22
+
+(conv
+ (least-common-bits example-input)) ; 9
 
 (def input
-  (str/split
-   (slurp (io/resource "3/input")) #"\n"))
+  (parse-input
+   (str/split
+    (slurp (io/resource "3/input")) #"\n")))
 
-(defn solve_part1 []
+(defn solve_part1 [input]
   (* (conv (most-common-bits input))
-     (conv (least-common-bits (most-common-bits input)))))
+     (conv (least-common-bits input))))
 
-(println "Day 3 #1: " (solve_part1))
+(println "Day 3 #1: " (solve_part1 input))
 
 ; part 2
 
-(defn parse-input [input]
-  (map parse input))
-
 (defn filter-nth-bit
-  [input pos val] 
+  [input pos val]
   (filter
    (fn [x]
      (= (nth x pos) val))
    input))
 
-(comment
-  ; example from the site
-  (def mcb (most-common-bits example-input))
-  (->
-   (filter-nth-bit (parse-input example-input) 0 (nth mcb 0))
-   (filter-nth-bit 1 (nth mcb 1))
-   (filter-nth-bit 2 (nth mcb 2))
-   (filter-nth-bit 3 (nth mcb 3))
-   (filter-nth-bit 4 (nth mcb 4)))
-  )
+(defn bitmask-filter [input-seq bitmask-fn]
+  (flatten
+   (reduce
+    (fn [coll pos]
+      (if (> (count coll) 1)
+        (let [val (nth (bitmask-fn coll) pos)]
+          (filter-nth-bit coll pos val))
+        coll))
+    input-seq
+    (range 0 (count (first input-seq))))))
 
-(defn with-indexes [seq] (map #(vector %1 %2) (range) seq))
+(bitmask-filter example-input most-common-bits)
 
-(defn bitmask-filter [input mask] 
-  (reduce
-   (fn [coll [pos val]]
-     (prn coll)
-     (filter-nth-bit coll pos val)) 
-   input
-   (with-indexes mask)))
+(bitmask-filter example-input least-common-bits)
 
-(bitmask-filter (parse-input example-input) mcb)
+(defn solve_part2 [input]
+  (* (conv (bitmask-filter input most-common-bits))
+     (conv (bitmask-filter input least-common-bits))))
+
+(solve_part2 example-input)
+
+(println "Day 3 #2: " (solve_part2 input))
